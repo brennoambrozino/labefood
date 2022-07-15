7f51b17ef8f8c9a63746ec751437a76b488f1242
brennoambrozino <brennoambrozino@gmail.com>
2022-07-14 22:04:42 -0300

    labefood

---
+++ labefood/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Router from "../src/routes/Router";
+import theme from "./constants/theme";
+import { ThemeProvider } from "@material-ui/core/styles";
+import GlobalState from "./context/global/GlobalState";
+export const App = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalState>
+        <Router />
+      </GlobalState>
+    </ThemeProvider>
+  );
+};
+export default App;
